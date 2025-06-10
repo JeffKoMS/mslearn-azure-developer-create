7d3985dd18dd1144fef13511148ce19f6d5a0ddc
JeffKoMS <jeffko@microsoft.com>
2025-06-10 13:54:18 -0700

    Added starter file

---
+++ dotnet/azure-events-messages/04-queue-storage/starter/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Threading.Tasks;
+
+// Create a unique name for the queue
+// TODO: Replace the <YOUR-STORAGE-ACCT-NAME> placeholder 
+string queueName = "myqueue-" + Guid.NewGuid().ToString();
+string storageAccountName = "storactname20135";
+
+// ADD CODE TO CREATE A QUEUE CLIENT AND CREATE A QUEUE
+
+
+
+// ADD CODE TO SEND AND LIST MESSAGES
+
+
+
+// ADD CODE TO UPDATE A MESSAGE AND LIST MESSAGES
+
+
+
+// ADD CODE TO DELETE MESSAGES AND THE QUEUE
+
