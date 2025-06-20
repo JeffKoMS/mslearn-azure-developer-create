49800b784e35de4a6cee2cd656a5f34ccb7c909a
JeffKoMS <jeffko@microsoft.com>
2025-06-20 14:39:33 -0700

    init

---
+++ dotnet/azure-secure-solutions/02-app-config-retrieve/finished/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Azure.Identity;
+
+//string endpoint = "YOUR_APP_CONFIGURATION_ENDPOINT"; // Replace with your Azure App Configuration endpoint
+string endpoint = "https://mslearnexercise.azconfig.io"; // Replace with your Azure App Configuration endpoint
+
+// Configure authentication options for connecting to Azure Key Vault
+DefaultAzureCredentialOptions credentialOptions = new()
+{
+    ExcludeEnvironmentCredential = true,
+    ExcludeManagedIdentityCredential = true
+};
+
+
+var builder = new ConfigurationBuilder();
+builder.AddAzureAppConfiguration(options =>
+{
+    
+    options.Connect(new Uri(endpoint), new DefaultAzureCredential(credentialOptions));
+});
+
+var config = builder.Build();
+Console.WriteLine(config["color"] ?? "Hello world!");
